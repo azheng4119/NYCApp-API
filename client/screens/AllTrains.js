97f4aee1f9391fe9df39dc1317a81e6b6de02fd2
ilumii <listeven073@gmail.com>
2020-03-30 14:38:37 -0400

    combine client

---
+++ client/screens/AllTrains.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import { Image, StyleSheet, ScrollView } from 'react-native';
+import { ListItem } from 'react-native-elements';
+import Images from '../assets/images';
+
+export default function AllTrains({
+	navigation
+}) {
+	const trains = ["1", "2", "3", "4", "5", "6", "7", "A", "B", "C", "D", "E", "F", "G", "J", "M", "N", "Q", "R", "S", "W", "Z"]
+	return (
+		<ScrollView>
+			{trains.map((train, i) => (
+				<ListItem
+					onPress={() => navigation.navigate('SingleTrain', { train, title: train })}
+					key={i}
+					leftAvatar={<Image source={Images[train]} style={styles.Avatar} />}
+					bottomDivider
+					chevron
+				/>
+			))}
+		</ScrollView>
+	);
+}
+
+const styles = StyleSheet.create({
+	Avatar: {
+		height: 40, 
+		width: 40,
+	},
+});
